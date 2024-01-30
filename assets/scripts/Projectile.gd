c93c21ef7992867c0ed398f313ee9b8610cbb8d8
nhennessy02 <nrh6850@rit.edu>
2024-01-30 14:38:57 -0500

    Made progress toward projectile firing

---
+++ assets/scripts/Projectile.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var direction
+var speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	speed = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#position = position + speed * global_rotation_degee * delta
+	pass
--- assets/scripts/wand.gd
+++ assets/scripts/wand.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+var projectileScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	projectileScene = preload("res://scenes/base_projectile.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	look_at(get_global_mouse_position())
-	rotate(deg_to_rad(90))
+	if Input.is_action_just_pressed("fire_wand"):
+		fire()
+
+func fire():
+	var projectile = projectileScene.instantiate()
+	get_tree().current_scene.add_child(projectile)
+	projectile.global_position = self.global_position
+	projectile.global_rotation = self.global_rotation
+	
