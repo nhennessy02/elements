89988f226f12ba0adcb3995a4b51a4a4c7c33cb1
nhennessy02 <nrh6850@rit.edu>
2024-01-31 10:26:20 -0500

    Made some extra changes :]

--- assets/scripts/Projectile.gd
+++ assets/scripts/Projectile.gd
@@ -5,10 +5,13 @@ var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed = 100
+	speed = 500
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#position = position + speed * global_rotation_degee * delta
 	position = position + Vector2.from_angle(global_rotation) * speed * delta; #Phillip - from_angle gives a unit vector in the direction from the angle given
 	pass
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
--- assets/scripts/wand.gd
+++ assets/scripts/wand.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var projectileScene
 
+@onready var spawnPoint = $ProjectileSpawnPoint
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	projectileScene = preload("res://scenes/base_projectile.tscn")
@@ -10,12 +12,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("fire_wand"):
+	if Input.is_action_just_pressed("fire_wand"): #left mouse click
 		fire()
 
 func fire():
 	var projectile = projectileScene.instantiate()
 	get_tree().current_scene.add_child(projectile)
-	projectile.global_position = self.global_position
+	projectile.global_position = spawnPoint.global_position #sets spawnpoint at the spawnpoint node
 	projectile.global_rotation = self.global_rotation
 	
