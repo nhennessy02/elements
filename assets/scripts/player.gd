94c5952c9998908c515884b1a179fa22414009e2
Phillip Brown <92740792+phbr1768@users.noreply.github.com>
2024-01-31 23:33:54 -0500

    Update player.gd
    Edited the CharacterBody2d template script to work for top down movement rather than side scrolling movement

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -6,10 +6,11 @@ const SPEED = 300.0
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down"); #Phillip - using get_vector instead of get_axis as it get all four directions
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity = direction * SPEED 
+		
 	move_and_slide()
