bae7df20b7fbe780b5d6fd362ba3f7bf435eae1a
doccsoccs <davinhow2@gmail.com>
2024-02-01 15:39:44 -0500

    Hold to Fire
    can hold leftmouse to fire

--- assets/scripts/wand.gd
+++ assets/scripts/wand.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+# Firing delay & hold to fire
+@export var fire_rate : float = 0.4 
+var fire_timer : float = 0
+var can_fire : bool = true
+
 @export var projectileScene : PackedScene
 
 @onready var spawnPoint = $ProjectileSpawnPoint
@@ -9,12 +14,25 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("fire_wand"): #left mouse click
+	
+	# Firing Cooldown
+	fire_timer += delta
+	if fire_timer >= fire_rate:
+		can_fire = true
+	else:
+		can_fire = false
+	
+	# Input event
+	if Input.is_action_pressed("fire_wand") and can_fire: #left mouse click
 		fire()
 
 func fire():
+	
+	# reset timer
+	fire_timer = 0
+	
 	var projectile = projectileScene.instantiate()
 	get_tree().current_scene.add_child(projectile)
 	projectile.global_position = spawnPoint.global_position #sets spawnpoint at the spawnpoint node
