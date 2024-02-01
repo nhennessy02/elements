076fe77a487f29edbd5484be17baec8ec0097b52
doccsoccs <davinhow2@gmail.com>
2024-02-01 15:48:55 -0500

    Enemies Chase Player
    enemies have a reference to player and can chase

--- assets/scripts/enemy.gd
+++ assets/scripts/enemy.gd
@@ -2,11 +2,21 @@ extends CharacterBody2D
 
 # Used for every enemy
 # Change these variables in the Inspector
-@export var speed : float = 300.0
+@export var speed : float = 200.0
 @export var damage : int = 5
 
+# We assign this in _ready()
+var player
+
+func _ready():
+	# Finds the player and gets a reference to it
+	if player == null: 
+		player = get_tree().get_nodes_in_group("Player")[0]
+
 func _physics_process(delta):
-	pass
+	if player != null:
+		velocity = position.direction_to(player.position) * speed
+		move_and_slide()
 
 # Damages the player when they enter the enemy's hitbox
 func _on_hit_box_body_entered(body):
