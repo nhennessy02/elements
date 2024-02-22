e8db9028342f96a5baf21a164632ee3cfa58cfcd
doccsoccs <davinhow2@gmail.com>
2024-02-21 19:06:06 -0500

    minimap
    minimap now displays player and enemies

--- scripts/EntityManager.gd
+++ scripts/EntityManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # Entities
-var entity_array : Array
+var entity_array : Array # not used currently
 
 # Enemy Object References
 var enemy_array : Array : 
@@ -12,11 +12,17 @@ var enemy_projectiles : Array
 var player
 var player_projectiles : Array
 
+# Minimap
+@onready var minimap = $Minimap
+
 func _ready():
 	player = get_tree().get_nodes_in_group("Player")[0]
 
 # Functions to adjust the enemy array
 func append_enemy(enemy: Object):
+	minimap.create_indicator()
 	enemy_array.append(enemy)
 func remove_enemy(enemy: Object):
+	minimap.remove_indicator(enemy)
 	enemy_array.erase(enemy)
+
