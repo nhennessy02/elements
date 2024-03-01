90fa8c4a5562d6a34e3cec93fdaac19d25e3165e
doccsoccs <davinhow2@gmail.com>
2024-03-01 11:52:33 -0500

    collisions working, avoid not

--- scripts/TestEnemy.gd
+++ scripts/TestEnemy.gd
@@ -2,10 +2,10 @@ extends Node
 
 # Tweak to adjust movement
 @export var seek_weight : float = 1
-@export var separate_weight : float = 5
+@export var separate_weight : float = 10
 @export var wander_weight : float = 1
 @export var flee_weight : float = 1
-@export var avoid_weight : float = 10
+@export var avoid_weight : float = 25
 
 # Outline behaviors
 func calc_steering_forces():	
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,14 +2,12 @@ class_name Enemy extends CharacterBody2D
 
 # Used for every enemy
 # Change these variables in the Inspector
-@export var speed : float = 200.0
+@export var speed : float = 150.0
 @export var damage : int = 1
 
 # We assign this in _ready()
 var player:
 	get: return player
-
-@onready var physics_object = $PhysicsObject
 
 # Gives references to other entities
 var entity_manager
@@ -24,12 +22,15 @@ var wander_pos : Vector2
 var wander_angle : float
 
 # Avoid Obstacle Variables
-var avoid_future_time : float = 0.5
-var avoid_radius : float = 50.0
+var avoid_future_time : float = 0.1
+var avoid_radius : float = 500.0
 
 # Used to calculate movement/forces
+var direction : Vector2 = Vector2.ZERO
+var acceleration : Vector2 = Vector2.ZERO
 var total_steering_force : Vector2 = Vector2.ZERO
-@export var max_force : Vector2 = Vector2(200.0, 200.0)
+@export var max_force : Vector2 = Vector2(150.0, 150.0)
+@export var mass : float = 1.0
 
 func _ready(): # LACKS DEFAULTS IN CASE OF MISSING PLAYER OR ENTITY MANAGER
 	entity_manager = get_tree().get_first_node_in_group("EntityManager")
@@ -42,12 +43,29 @@ func _ready(): # LACKS DEFAULTS IN CASE OF MISSING PLAYER OR ENTITY MANAGER
 	# Finds the player and gets a reference to it
 	player = entity_manager.player
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	# Calc velocity each frame
+	velocity += acceleration * delta
+	position += velocity * delta
+	
+	# Get direction based on velocity
+	direction = velocity.normalized()
+	
+	# Zero acceleration
+	acceleration = Vector2.ZERO	
+	
 	# Apply all steering forces to the enemy's movements
 	total_steering_force = Vector2.ZERO
 	get_node("Behavior").calc_steering_forces()
 	total_steering_force = total_steering_force.clamp(-max_force, max_force)
-	physics_object.apply_force(total_steering_force)
+	apply_force(total_steering_force)
+	
+	# Activate Collisions with Tilemap
+	move_and_slide()
+
+# Applies movement to the enemy
+func apply_force(force: Vector2):
+	acceleration += force * mass
 
 # Damages the player when they enter the enemy's hitbox
 func _on_hit_box_body_entered(body):
@@ -58,24 +76,25 @@ func _on_hit_box_body_entered(body):
 # Movement function: Enemies move towards player position
 func seek(seek_target_pos: Vector2):
 	# Get desired velocity
-	var desired_velocity : Vector2 = seek_target_pos - physics_object.position
+	var desired_velocity : Vector2 = seek_target_pos - position
 	
 	# Scale to enemy's speed
 	desired_velocity = desired_velocity.normalized() * speed
 	
 	# Calculate and return the force needed to move towards target
-	var seekForce : Vector2 = desired_velocity - physics_object.velocity
+	var seekForce : Vector2 = desired_velocity - velocity
 	return seekForce;
 
+# Move away from a target
 func flee(flee_target_pos: Vector2):
 	# Determind desired flee velocity
-	var desired_velocity : Vector2 = physics_object.position - flee_target_pos
+	var desired_velocity : Vector2 = position - flee_target_pos
 	
 	# Scale to enemy speed
 	desired_velocity = desired_velocity.normalized() * speed;
 	
 	# Calculate and return the fleeing force
-	var flee_force : Vector2 = desired_velocity - physics_object.velocity
+	var flee_force : Vector2 = desired_velocity - velocity
 	return flee_force
 
 # Movement function: Enemies separate from each other
@@ -86,7 +105,7 @@ func separate():
 	# Flee all other enemies
 	for enemy in entity_manager.enemy_array:
 		if enemy != self:
-			sqr_dist = (physics_object.position - enemy.position).length()
+			sqr_dist = (position - enemy.position).length()
 			
 			# Scale flee based on distance
 			if sqr_dist != 0:
@@ -102,26 +121,27 @@ func avoid_obstacle():
 	var right_dot : float
 	var forward_dot : float
 	
-	var future_pos = calc_future_position(avoid_future_time)
-	var future_sqr_dist : float = (future_pos - physics_object.position).length()
+	var future_pos : Vector2 = calc_future_position(avoid_future_time)
+	var future_sqr_dist : float = (future_pos - position).length()
 	future_sqr_dist += avoid_radius
 	
 	# Avoids all obstacles with varying weight depending on their distance
 	for obstacle in entity_manager.obstacles_array:
-		vec_to_obstacle = obstacle.position - physics_object.position
-		forward_dot = vec_to_obstacle.dot(physics_object.position)
+		vec_to_obstacle = obstacle.position - position
+		forward_dot = vec_to_obstacle.dot(position)
 		
 		if forward_dot >= 0 and forward_dot * forward_dot <= future_sqr_dist:
 			right_dot = vec_to_obstacle.dot(Vector2.RIGHT)
 			
 			if abs(right_dot) <= avoid_radius:
 				if right_dot < 0: # TURN RIGHT
-					avoid_force += Vector2.RIGHT * speed * (1/forward_dot)
+					avoid_force += Vector2.RIGHT * max_force * (1/forward_dot)
 				else: # TURN LEFT
-					avoid_force += -Vector2.RIGHT * speed * (1/forward_dot)
+					avoid_force += -Vector2.RIGHT * max_force * (1/forward_dot)
 	
 	return avoid_force
 
+# Wanders in a random direction
 func wander(future_time: float = 0.5, wander_radius: float = 2.0):
 	wander_pos = calc_future_position(future_time)
 	
@@ -130,13 +150,16 @@ func wander(future_time: float = 0.5, wander_radius: float = 2.0):
 	
 	return seek(wander_pos)
 
+# Change the angle at which the agent is wandering
 func change_wander_angle():
 	wander_angle = randf_range(0.0, 360.00)
 	wander_angle = deg_to_rad(wander_angle)
 
-func calc_future_position(time: float = 0.5):
-	return physics_object.position + physics_object.velocity * time
+# find where the agent will be after X time
+func calc_future_position(time: float = 0.1):
+	return position + velocity * time
 
+# NEEDS TO PLAY A DEATH ANIMATION FIRST
 func die():
 	entity_manager.remove_enemy(self)
 	queue_free()
