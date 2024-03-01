ed8135fcc4fc2b7476ec57408b248d7d430c5893
Phillip Brown <92740792+phbr1768@users.noreply.github.com>
2024-03-01 01:07:18 -0500

    convalescence kinda done

--- scripts/spells/convalescence.gd
+++ scripts/spells/convalescence.gd
@@ -1,19 +1,26 @@
 extends Node
 
 @onready var timer = $ActivationTimer
+@onready var ring = $InnerRing
 var player
 var healAmount = 5
+var startTime
+var scaleFactor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node("../Player")
-	self.rotation = 0;
+	startTime = timer.time_left
+	self.global_rotation = 0;
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	scaleFactor = (startTime - timer.time_left) / startTime
 	self.position = player.position
+	ring.scale.x = 2 * scaleFactor
+	ring.scale.y = 2 * scaleFactor
 	if timer.time_left > 0 and Input.is_action_just_released("fire_wand"):
 		fizzle()
 		pass
