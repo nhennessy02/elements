5d8193ab111ece04927612444c2dd6fd1b4665a2
doccsoccs <davinhow2@gmail.com>
2024-03-07 17:12:55 -0500

    added new enemy type

---
+++ scripts/enemy behavior/testenemy2_behavior.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Tweak to adjust movement
+@export var seek_weight : float = 1
+@export var separate_weight : float = 10
+@export var wander_weight : float = 1
+@export var flee_weight : float = 1
+@export var avoid_weight : float = 5
+
+# Outline behaviors
+func calc_steering_forces():	
+	owner.total_steering_force += owner.pathfinding() * seek_weight
+	owner.total_steering_force += owner.separate() * separate_weight
