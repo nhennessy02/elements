ad9d097853bfcc9d48780d7c3a129dd769a10fad
nhennessy02 <nrh6850@rit.edu>
2024-03-22 12:27:03 -0400

    More cooldown timer progress

--- prefabs/player/CooldownBar.gd
+++ prefabs/player/CooldownBar.gd
@@ -1,13 +1,20 @@
 extends ProgressBar
-@onready var comboTimer = $"../Inventory/Timer"
-
+@onready var timer = $"../Wand".cooldownTimer
+var cooldownValue
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	cooldownValue = timer.get_wait_time()
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsedaw time since the previous frame.
 func _process(delta):
+	print(value)
+	value = timer.get_time_left()
 	pass
+	
 
-
+func _on_inventory_combo_created(spellName, useRate, scene, wandColor):
+	cooldownValue = timer.get_wait_time()
+	max_value = cooldownValue
+	value = cooldownValue
