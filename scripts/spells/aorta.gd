b0c9e70f5850c50e75e881ac5910fe2f68e09b17
Phillip Brown (RIT Student) <pkb5886@rit.edu>
2024-04-09 13:29:06 -0400

    copied hemomancy to aorta

---
+++ scripts/spells/aorta.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var speed : float = 700
+@export var damage : int = 3
+@export var cooldown : float = 1
+var wand
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	wand = get_node("../Player/Wand")
+	wand.startFireAnimation()
+	wand.startCooldown(cooldown)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = position + Vector2.from_angle(global_rotation) * speed * delta;
+
+func _on_area_2d_body_entered(body):
+	for child in body.get_children():
+		if child is Damageable:
+			child.hit(damage);
+			EffectManager.attach_effect(body,Effects.BLEED,3)
+
+func _on_lifetime_timeout():
+	queue_free()
