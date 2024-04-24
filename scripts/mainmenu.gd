3a895be76315bcf8889a85a2db534bdfbd1355d1
nhennessy02 <nrh6850@rit.edu>
2024-04-24 10:48:56 -0400

    Added a tutorial scene

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 # PLAY BUTTON
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file.call_deferred("res://scenes/subarea.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/tutorial.tscn")
 
 # QUIT BUTTON
 func _on_quit_button_pressed():
